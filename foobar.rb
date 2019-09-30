1dac700d7dee5dd8ca4a06276a77850ee8f97efb
henryleou <henryleou@berkeley.edu>
2019-09-30 03:54:28 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,13 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    r = 0
+    a.uniq.each { |i|
+    	i = i.to_i
+      if i + 2 <= 10 && i % 2 == 0 
+        then r += i + 2 
+      end
+    }
+    r
   end
 end
--- hw2.rb
+++ hw2.rb
@@ -1,13 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort!.each_with_index {|element, i| a[i] = element + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -46,4 +49,5 @@ def scrabble(word)
     z: 10
   }
   # Q5 CODE HERE
+  word.split('').map{ |i| values[i.intern] }.sum
 end
